39c2e65af5cc0ad676b795f3a1df33191d14d3b8
Jad Bizri <jb4954@hotmail.com>
2025-02-11 16:51:30 -0500

    Structured API endpoints

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from '../core/services/app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // Return registration details of an ENS name
+  @Get("/ens/:name/details")
+  getRegistration(@Param('name') name: string) {
+    
+  }
+
+  //Fetch metadata changes over time
+  @Get('/ens/:name/metadata')
+  getMetadata(@Param('name') name: string) {
+
+  }
+
+  // Retrieve the transfer history of an ENS name
+  @Get('/ens/:name/history')
+  getTransferHistory(@Param('name') name: string) {
+
+  }
+
+  // List all ENS names owned by a specific address
+  @Get('/ens/owned/:address')
+  getEnsNames(@Param('address') address: string) {
+    
   }
 }
